bfd4b7adcdb34502be107a67666c086d5dfec667
Etienne <etiennesene.ds@gmail.com>
2025-06-11 17:34:46 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import '../dist/css/adminlte.css'
+// import '../dist/css/adminlte.css'
+import "admin-lte/dist/css/adminlte.css";
+//import "admin-lte/dist/js/adminlte.js";
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
